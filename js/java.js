9b3ba0048ac03efd2c10260e0c09a982797bde3c
Haji Bamsi <hibamsi@gmail.com>
2022-04-05 12:06:58 +0300

    add style to mobile menu

--- js/java.js
+++ js/java.js
@@ -1,7 +1,7 @@
 let hamburger = document.querySelector(".toggle-icon");
 let close_icon = document.querySelector(".close-icon");
 let nav_menu = document.querySelector(".nav-desktop");
-let main_container = document.querySelector(".main-container");
+let logo_brand = document.querySelector(".logo-brand");
 let menu_items = document.querySelectorAll(".nav-link");
 
 [hamburger, close_icon].forEach((item) => {
@@ -14,12 +14,12 @@ function showMenu() {
   nav_menu.classList.toggle("nav-mobile");
   hamburger.classList.toggle("hide");
   close_icon.classList.toggle("show");
-  main_container.classList.toggle("menu-background");
+  logo_brand.classList.toggle("hide");
 }
 
 function hideMenu() {
   hamburger.classList.remove("hide");
   close_icon.classList.remove("show");
-  main_container.classList.remove("menu-background");
+  logo_brand.classList.remove("hide");
   nav_menu.classList.remove("nav-mobile");
 }
