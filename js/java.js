8b260941e2cc6dd37f1d6bffd81f3090eeeafd73
Haji Bamsi <hibamsi@gmail.com>
2022-04-06 13:55:28 +0300

    fix linter errors

--- js/java.js
+++ js/java.js
@@ -1,160 +1,166 @@
-const hamburger = document.querySelector(".toggle-icon");
-const closeIcon = document.querySelector(".close-icon");
-const navMenu = document.querySelector(".nav-desktop");
-const logoBrand = document.querySelector(".logo-brand");
-const menuItems = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector('.toggle-icon');
+const closeIcon = document.querySelector('.close-icon');
+const navMenu = document.querySelector('.nav-desktop');
+const logoBrand = document.querySelector('.logo-brand');
+const menuItems = document.querySelectorAll('.nav-link');
+const iconClose = document.querySelectorAll('.icon-close');
+const projectLink = document.querySelectorAll('.link-project');
 
 function showMenu() {
-  navMenu.classList.toggle("nav-mobile");
-  hamburger.classList.toggle("hide");
-  closeIcon.classList.toggle("show");
-  logoBrand.classList.toggle("hide");
+  navMenu.classList.toggle('nav-mobile');
+  hamburger.classList.toggle('hide');
+  closeIcon.classList.toggle('show');
+  logoBrand.classList.toggle('hide');
 }
 
 function hideMenu() {
-  hamburger.classList.remove("hide");
-  closeIcon.classList.remove("show");
-  logoBrand.classList.remove("hide");
-  navMenu.classList.remove("nav-mobile");
+  hamburger.classList.remove('hide');
+  closeIcon.classList.remove('show');
+  logoBrand.classList.remove('hide');
+  navMenu.classList.remove('nav-mobile');
 }
 
 [hamburger, closeIcon].forEach((item) => {
-  item.addEventListener("click", showMenu);
+  item.addEventListener('click', showMenu);
 });
 
-menuItems.forEach((n) => n.addEventListener("click", hideMenu));
+menuItems.forEach((n) => n.addEventListener('click', hideMenu));
 
 /** define project object */
-let projects = [
+const projects = [
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
-    image: "images/image.svg",
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.',
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
-    image: "images/image.svg",
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.',
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
-    image: "images/image.svg",
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.',
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
-    image: "images/image.svg",
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.',
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
-    image: "images/image.svg",
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.',
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
+    name: 'Multi-Post Stories Gain+Glory',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: "images/image.svg",
+    image: 'images/image.svg',
     technologies: [
-      { name: "Ruby on rails" },
-      { name: "CSS" },
-      { name: "Javascript" },
-      { name: "Html" },
+      { name: 'Ruby on rails' },
+      { name: 'CSS' },
+      { name: 'Javascript' },
+      { name: 'Html' },
     ],
-    live: "#",
-    source: "#",
+    live: 'https://bamsi.github.io/portfolio/',
+    source: 'https://github.com/bamsi/portfolio',
   },
 ];
 
 /** function to close model */
 function closePopup() {
-  const model = document.getElementById("model-container");
-  const container = document.querySelector(".main-container");
-  container.classList.remove("hide-main");
-  model.classList.remove("show-popup");
+  const model = document.getElementById('model-container');
+  const container = document.querySelector('.main-container');
+  container.classList.remove('hide-main');
+  model.classList.remove('show-popup');
 }
 
 function loadProjects() {
-  const list = document.getElementById("project-list");
-  let projectList = "";
-  for (item of projects) {
+  const list = document.getElementById('project-list');
+  let projectList = '';
+  projects.forEach((item) => {
     projectList += `<li class="list-item">
-                     <div class="image-placeholder"></div>
-                     <div class="image-desc">
-                        <h3>${item.name}</h3>
-                    <div class="buttons">`;
-    for (language of item.technologies) {
+    <div class="image-placeholder"></div>
+    <div class="image-desc">
+       <h3>${item.name}</h3>
+   <div class="buttons">`;
+    item.technologies.forEach((language) => {
       projectList += `<div>${language.name}</div>`;
-    }
+    });
     projectList += `</div>
-                     <div class="button-link">
-                        <button class="button" type="button" onclick="openProject(item)">See Project</button>
-                      </div>
-                     </div>
-                    </li>`;
-  }
+    <div class="button-link">
+       <button class="button link-project" type="button" onclick="openProject()">See Project</button>
+     </div>
+    </div>
+   </li>`;
+  });
+
   list.innerHTML = projectList;
 }
 window.onload = loadProjects();
 
-function openProject(item) {
-  const model = document.getElementById("model-container");
-  const container = document.querySelector(".main-container");
+function openProject() {
+  const model = document.getElementById('model-container');
+  const container = document.querySelector('.main-container');
+  const item = projects[0];
+
   let innerHtml = `<div class="model">
                      <div class="model-placeholder">
-                       <image src="../images/cancel-popup.svg" alt="Cancel" onClick="closePopup()" />
+                       <span class="icon-close" onclick="closePopup()"></span>
                      </div>
                       <h1 class="project-title">${item.name}</h1>
                       <div class="buttons">`;
-  for (language of item.technologies) {
+
+  item.technologies.forEach((language) => {
     innerHtml += `<div>${language.name}</div>`;
-  }
+  });
   innerHtml += `</div>
                 <p class="model-description">${item.description}</p>
                 <div class="project-buttons">
@@ -163,6 +169,9 @@ function openProject(item) {
                 </div>
                 </div>`;
   model.innerHTML = innerHtml;
-  container.classList.add("hide-main");
-  model.classList.add("show-popup");
+  container.classList.add('hide-main');
+  model.classList.add('show-popup');
 }
+
+iconClose.forEach((n) => n.addEventListener('click', closePopup));
+projectLink.forEach((n) => n.addEventListener('click', openProject));
