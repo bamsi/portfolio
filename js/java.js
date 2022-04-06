1e6d66765d7b0dc434817ea468c8795e57fa00bc
Haji Bamsi <hibamsi@gmail.com>
2022-04-06 11:21:30 +0300

    add project object to javascript

--- js/java.js
+++ js/java.js
@@ -23,3 +23,91 @@ function hideMenu() {
 });
 
 menuItems.forEach((n) => n.addEventListener("click", hideMenu));
+
+/** define project object */
+let projects = [
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error,doloremque reprehenderit debitis quam molestias vel, consequuntur autem neque temporibus, magni id. Dolorum vero voluptatibus aspernatur animi,eos nesciunt. Exercitationem.",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: "images/image.svg",
+    technologies: [
+      { name: "Ruby on rails" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "Html" },
+    ],
+    live: "#",
+    source: "#",
+  },
+];
