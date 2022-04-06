2fca97208fcff1339ee569a02c7b82e6edeab64c
Haji Bamsi <hibamsi@gmail.com>
2022-04-06 11:19:46 +0300

    add model container to the index page

--- js/java.js
+++ js/java.js
@@ -1,25 +1,25 @@
-const hamburger = document.querySelector('.toggle-icon');
-const closeIcon = document.querySelector('.close-icon');
-const navMenu = document.querySelector('.nav-desktop');
-const logoBrand = document.querySelector('.logo-brand');
-const menuItems = document.querySelectorAll('.nav-link');
+const hamburger = document.querySelector(".toggle-icon");
+const closeIcon = document.querySelector(".close-icon");
+const navMenu = document.querySelector(".nav-desktop");
+const logoBrand = document.querySelector(".logo-brand");
+const menuItems = document.querySelectorAll(".nav-link");
 
 function showMenu() {
-  navMenu.classList.toggle('nav-mobile');
-  hamburger.classList.toggle('hide');
-  closeIcon.classList.toggle('show');
-  logoBrand.classList.toggle('hide');
+  navMenu.classList.toggle("nav-mobile");
+  hamburger.classList.toggle("hide");
+  closeIcon.classList.toggle("show");
+  logoBrand.classList.toggle("hide");
 }
 
 function hideMenu() {
-  hamburger.classList.remove('hide');
-  closeIcon.classList.remove('show');
-  logoBrand.classList.remove('hide');
-  navMenu.classList.remove('nav-mobile');
+  hamburger.classList.remove("hide");
+  closeIcon.classList.remove("show");
+  logoBrand.classList.remove("hide");
+  navMenu.classList.remove("nav-mobile");
 }
 
 [hamburger, closeIcon].forEach((item) => {
-  item.addEventListener('click', showMenu);
+  item.addEventListener("click", showMenu);
 });
 
-menuItems.forEach((n) => n.addEventListener('click', hideMenu));
+menuItems.forEach((n) => n.addEventListener("click", hideMenu));
