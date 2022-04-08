e948a043a0f05cbe32f6944d4141c4cf85d12333
Haji Bamsi <hibamsi@gmail.com>
2022-04-08 19:58:27 +0300

    fix upper case characters validation

--- js/java.js
+++ js/java.js
@@ -181,7 +181,7 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
   const email = document.getElementById('email');
   const error = document.getElementById('error');
-  const pattern = /^([a-zd\.\-_]+)@([a-zd\-_]+).([a-z\-_]{2,8})(.[a-z\-_]{2,8})?$/;
+  const pattern = /^([a-zd.\-_]+)@([a-zd\-_]+).([a-z\-_]{2,8})(.[a-z\-_]{2,8})?$/;
   const message = 'You have entered an invalid email address!';
   if (!pattern.test(email.value)) {
     e.preventDefault();
